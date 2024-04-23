c775aa91eec7a07c611e531ab5d904995eedeb88
nabilarizqa <nabilarizqa@gmail.com>
2024-04-23 08:54:42 +0700

    login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DaftarLaporanComponent } from './daftar-laporan/daftar-laporan.component';
 import { DaftarSelesaiComponent } from './daftar-selesai/daftar-selesai.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes:  Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: '', component: DashboardComponent },
   { path: 'daftar-laporan', component: DaftarLaporanComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'daftar-selesai', component: DaftarSelesaiComponent },
 ];
