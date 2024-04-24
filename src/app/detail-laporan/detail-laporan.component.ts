3082be694f57a0dadd1c3311aba9926da8b3f5de
nabilarizqa <nabilarizqa@gmail.com>
2024-04-24 11:11:56 +0700

    detail-laporan

---
+++ src/app/detail-laporan/detail-laporan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-laporan',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-laporan.component.html',
+  styleUrl: './detail-laporan.component.css'
+})
+export class DetailLaporanComponent {
+
+}
