441eedb325457587083d8b90a291d7bf90b69e5d
Renata Rizki Rafi` Athallah <renatarizki35@gmail.com>
2024-05-06 08:44:16 +0700

    feat: daftar laporan selesai screen

--- src/app/daftar-selesai/daftar-selesai.component.ts
+++ src/app/daftar-selesai/daftar-selesai.component.ts
@@ -1,12 +1,60 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import axios from 'axios';
+import { AuthService } from '../auth.service';
+import { RouterLink } from '@angular/router';
+
+
+interface report {
+  reported_account_id: number;
+  account_number: string;
+  reports_count: number;
+  status: number;
+  admin: string;
+  time_finished: Date;
+}
 
 @Component({
   selector: 'app-daftar-selesai',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './daftar-selesai.component.html',
   styleUrl: './daftar-selesai.component.css'
 })
 export class DaftarSelesaiComponent {
+  constructor(private authService: AuthService) {
+    this.getReport();
+  }
+
+  report_list: report[] = [];
+
+  async getReport(){
+    try{
+      const token = this.authService.getToken();
+      const response = await axios.get('/api/reportedAcc/website', {headers: {"Authorization": "Bearer " + token}});
+      console.log(response.data.data);
+      this.report_list = response.data.data.map((item: any) => {
+        try {
+          return {
+            reported_account_id: item.reported_account_id,
+            account_number: item.account_number,
+            reports_count: item.reports_count,
+            status: item.status,
+            admin: item.admin,
+            // Konversi timestamp string ke objek Date
+            time_finished: new Date(item.time_finished)
+          };
+        } catch (error) {
+          console.error("Error converting timestamp:", error);
+          console.log("Invalid timestamp value:", item.time_finished);
+          // Jika terjadi kesalahan, kembalikan null atau nilai default 
+          return null; // atau nilai default lainnya
+        }
+      }).filter((item: any) => item !== null); // Filter nilai yang null
+  
+    }catch (error) {
+      console.log(error);
+    }
+  }
 
 }
